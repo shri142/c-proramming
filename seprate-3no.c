4ca39f780b458f564178c488ab94f4be78548ee6
shri142 <shrikrishna2002umbare@gmail.com>
2022-05-08 22:57:43 +0530

    updated code

--- seprate-3no.c
+++ seprate-3no.c
@@ -1,17 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-   	int n,a,b,c,d,p,q;
-   	
-   	printf("enter 3 digit no\n");
-   	scanf("%d",&n);
-    	a=n%10;
-   	    b=n/10;
-     	p=b%10;
-    	c=b/10;
-   	    q=c%10;
-    printf("the 3 nos after seperating are %d",q);
-    printf(" %d",p);
-	printf(" %d",a);
-	
-    return 0;}
+	int n, a, b, c, d, p, q;
+
+	printf("enter 3 digit no\n");
+	scanf("%d", &n);
+	a = n % 10;
+	b = n / 10;
+	p = b % 10;
+	c = b / 10;
+	q = c % 10;
+	printf("the 3 nos after seperating are %d", q);
+	printf(" %d", p);
+	printf(" %d", a);
+
+	return 0;
+}
